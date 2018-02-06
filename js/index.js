0a06d65e7f4148ee6d354935fa56c01ecccfb7ac
henghenghahadada <xing19931122@sina.com>
2018-02-06 12:25:00 +0800

    dd
    asda

--- js/index.js
+++ js/index.js
@@ -1,46 +1,6 @@
 $(function(){
     $('#dowebok').fullpage({
         anchors:['page1','page2','page3','page4'],
-        loopBottom:true,
-        afterLoad:function(Link,index){
-            switch (index){
-                case 1:
-                    $('.zg-main1 b').animate({'backgroundPosition':'center'},800);
-                    $('.download').animate({'marginLeft':'0.4rem'},800);
-                    break;
-                case 2:
-                    $('.zg-main2 b').animate({'marginTop':'0'},800);
-                    $('.download').animate({'marginLeft':'0.4rem'},800);
-                    break;
-                case 3:
-                    $('.zg-main3 b').animate({'marginTop':'0'},800);
-                    $('.download').animate({'marginLeft':'0.4rem'},800);
-                    break;
-                case 4:
-                    $('.zg-main4 b').animate({'marginTop':'0'},800);
-                    $('.download').animate({'marginLeft':'0.4rem'},800);
-                    break;
-            }
-        }
-        // onLeave:function (link,index) {
-        //     switch (index){
-        //         case 1:
-        //             $('.zg-main1 b').animate({'marginTop':'-1rem'},200);
-        //             $('.download').animate({'marginLeft':'-0.4rem'},200);
-        //             break;
-        //         case 2:
-        //             $('.zg-main2 b').animate({'marginTop':'-1rem'},200);
-        //             $('.download').animate({'marginLeft':'1rem'},200);
-        //             break;
-        //         case 3:
-        //             $('.zg-main3 b').animate({'marginTop':'-1rem'},200);
-        //             $('.download').animate({'marginLeft':'-0.4rem'},200);
-        //             break;
-        //         case 4:
-        //             $('.zg-main4 b').animate({'marginTop':'-1rem'},200);
-        //             $('.download').animate({'marginLeft':'1rem'},200);
-        //             break;
-        //     }
-        // }
+        loopBottom:true
     })
 })
