a7c6f56c4d75a61b2dfb943ca96a91bb6014cbe4
henghenghahadada <xing19931122@sina.com>
2018-02-06 12:13:38 +0800

    v2.1
    v2.1

--- js/index.js
+++ js/index.js
@@ -21,26 +21,26 @@ $(function(){
                     $('.download').animate({'marginLeft':'0.4rem'},800);
                     break;
             }
-        },
-        onLeave:function (link,index) {
-            switch (index){
-                case 1:
-                    $('.zg-main1 b').animate({'marginTop':'-1rem'},200);
-                    $('.download').animate({'marginLeft':'-0.4rem'},200);
-                    break;
-                case 2:
-                    $('.zg-main2 b').animate({'marginTop':'-1rem'},200);
-                    $('.download').animate({'marginLeft':'1rem'},200);
-                    break;
-                case 3:
-                    $('.zg-main3 b').animate({'marginTop':'-1rem'},200);
-                    $('.download').animate({'marginLeft':'-0.4rem'},200);
-                    break;
-                case 4:
-                    $('.zg-main4 b').animate({'marginTop':'-1rem'},200);
-                    $('.download').animate({'marginLeft':'1rem'},200);
-                    break;
-            }
         }
+        // onLeave:function (link,index) {
+        //     switch (index){
+        //         case 1:
+        //             $('.zg-main1 b').animate({'marginTop':'-1rem'},200);
+        //             $('.download').animate({'marginLeft':'-0.4rem'},200);
+        //             break;
+        //         case 2:
+        //             $('.zg-main2 b').animate({'marginTop':'-1rem'},200);
+        //             $('.download').animate({'marginLeft':'1rem'},200);
+        //             break;
+        //         case 3:
+        //             $('.zg-main3 b').animate({'marginTop':'-1rem'},200);
+        //             $('.download').animate({'marginLeft':'-0.4rem'},200);
+        //             break;
+        //         case 4:
+        //             $('.zg-main4 b').animate({'marginTop':'-1rem'},200);
+        //             $('.download').animate({'marginLeft':'1rem'},200);
+        //             break;
+        //     }
+        // }
     })
 })
